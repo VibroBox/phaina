e370525849d838adabc449eced82c02aab02ea4c
Alexander Zolotarev <me@alex.bio>
2017-06-06 18:31:11 +0300

    Links titles for aside.

--- include/aside.php
+++ include/aside.php
@@ -1,6 +1,6 @@
 <aside>
   <?php if (PageFile() == 'technology.php') : ?>
-  <a class="jumpToTOC" href="<?= T(['en'=>'#table_of_contents', 'ru'=>'#содержание']) ?>">→</a>
+  <a class="jumpToTOC" title="<?= T('goToTableOfContents') ?>" href="<?= T(['en'=>'#table_of_contents', 'ru'=>'#содержание']) ?>">→</a>
   <?php endif ?>
-  <a class="feedback" href="mailto:info@vibrobox.com?subject=<?= T('feedback').' / '.PageTitle() ?>"><?= T('feedback') ?></a>
+  <a class="feedback" title="<?= T('feedback') ?>" href="mailto:info@vibrobox.com?subject=<?= T('feedback').' / '.PageTitle() ?>"><?= T('feedback') ?></a>
 </aside>
