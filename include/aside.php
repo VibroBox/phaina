8558191e12887c3ade6df3153902d365d7e12ab5
Alexander Zolotarev <me@alex.bio>
2017-06-06 15:41:00 +0300

    Minor aside refactoring and TOC link for the technology page.

--- include/aside.php
+++ include/aside.php
@@ -1,3 +1,6 @@
 <aside>
+  <?php if (PageFile() == 'technology.php') : ?>
+  <a class="jumpToTOC" href="<?= T(['en'=>'#table_of_contents', 'ru'=>'#содержание']) ?>">→</a>
+  <?php endif ?>
   <a class="feedback" href="mailto:info@vibrobox.com?subject=<?= T('feedback').' / '.PageTitle() ?>"><?= T('feedback') ?></a>
 </aside>
